9bfb6616509c3ad94db6b012f888314d81a17bbf
Kacper Stroisz <kacperstroisz@wp.pl>
2018-02-04 21:21:29 +0100

    Change .js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
-var element = document.createElement('li');
-
-var one = document.getElementsByTagName('li');
-console.log(one.length);
-
-element.innerHTML = 'item';
-list.appendChild(element);
+	var element = document.createElement('li');
+	var listItems = document.getElementsByTagName('li');
+	console.log(listItems.length);
+	
+	var i = 1;
+	element.innerHTML = 'item' + i++;
+	list.appendChild(element);
 });
